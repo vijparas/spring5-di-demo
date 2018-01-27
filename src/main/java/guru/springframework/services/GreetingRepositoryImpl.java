946772639bca06d9a94150073ee91153d922f024
Paras <vijparas@gmail.com>
2018-01-27 14:06:37 +0530

    Added autowired annotation in primary german greeting service class so that it injects GreetingRepositoryImpl and also marked GreetingRepositoryImpl as service

--- src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -1,8 +1,12 @@
 package guru.springframework.services;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by jt on 5/24/17.
  */
+@Service
 public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Primary
 @Profile("de")
 public class PrimaryGermanGreetingService implements GreetingService {
-
+    @Autowired
     private GreetingRepository greetingRepository;
 
     @Override
